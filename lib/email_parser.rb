70afaceaa6403efd54f4246479b51df14aa346a3
Tristan Brown <tgbrown@stanford.edu>
2018-09-18 06:37:35 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,11 +6,14 @@ class EmailParser
   end
 
   def parse
-    email_array = emails.split(/,\s|\s/)
-    email_array.uniq 
+    emails_array = emails.split(/,\s|\s/)
+    emails_array.uniq
   end
 
 end
+
+emails = "avi@test.com, arel@test.com"
+EmailParser.new(emails).parse
 
 # Build a class EmailParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
